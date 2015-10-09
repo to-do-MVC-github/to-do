32a1076fc3d47d2121b0b7a046b60e29ad841bbc
Garrett Sand <garrettsand@gmail.com>
2015-10-08 18:16:11 -0700

    Put titles of issues from user's github json data in new array and return it

--- js/issues.js
+++ js/issues.js
@@ -3,14 +3,24 @@ $(document).ready(function ($) {
   var issues = {
     fetchIssues: function() {
       var token = window.prompt('Give me your soul!!!');
-      var baseURL = "https://api.github.com/issues"
-      var params = "?access_token=" + token
-      var url = baseURL + params
+      var baseURL = "https://api.github.com/issues";
+      var params = "?access_token=" + token;
+      var url = baseURL + params;
       $.getJSON(url, function(data) {
-        console.log(data);
+        // console.log(data);
+        issues.splitIssues(data);
       });
-    }
+    },
     // make new to-do items from each issue returned
+    splitIssues: function(issuesData) {
+      var issuesTitles = [];
+      for (var i = 0; i < issuesData.length; i++) {
+        // console.log(issuesData[i].title);
+        issuesTitles.unshift(issuesData[i].title);
+      }
+      console.log(issuesTitles);
+      return issuesTitles;
+    }   
   };
   $issuesButton.click(issues.fetchIssues);
 });
